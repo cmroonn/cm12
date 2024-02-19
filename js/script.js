dbd60e0d88f32c24f83dfd0923dfe33800083681
cmroonn <fallgetell@gmail.com>
2024-02-19 14:30:11 +0300

    fixes

--- js/script.js
+++ js/script.js
@@ -578,7 +578,7 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
       var _elems2 = document.querySelectorAll(".faq__buttons__text");
       _elems2.forEach(function (el) {
-        el.dataset.height = Number(el.offsetHeight) + 15;
+        el.dataset.height = Number(el.querySelector('p').offsetHeight) + 40;
         el.style.height = "0px";
         el.style.padding = "0px";
       });
@@ -588,22 +588,22 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         btn.addEventListener("click", function () {
           if (btn.classList.contains("active")) {
-            btn.classList.remove("active");
-            _elems2.forEach(function (el) {
-              if (btn.dataset.id === el.dataset.id) {
-                el.style.height = "0px";
-                setTimeout(function () {
-                  el.classList.remove("active");
-                }, 200);
-              }
-            });
+            // btn.classList.remove("active");
+            // _elems2.forEach(function (el) {
+            //   if (btn.dataset.id === el.dataset.id) {
+            //     el.style.height = "0px";
+            //     setTimeout(function () {
+            //       el.classList.remove("active");
+            //     }, 200);
+            //   }
+            // });
           } else {
-            _buttons10.forEach(function (btn2) {
-              return btn2.classList.remove("active");
-            });
+            // _buttons10.forEach(function (btn2) {
+            //   return btn2.classList.remove("active");
+            // });
             _elems2.forEach(function (el) {
-              el.style.height = "0px";
-              el.classList.remove("active");
+              // el.style.height = "0px";
+              // el.classList.remove("active");
               if (btn.dataset.id === el.dataset.id) {
                 el.style.height = el.dataset.height + "px";
                 el.classList.add("active");
